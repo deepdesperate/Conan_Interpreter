3d1e9f847efeee8e286f4f18b2bbfcbce43810cf
Naman Deep <namandeep444@gmail.com>
2024-09-19 10:03:58 +0530

    Completing simple lexer functionality understanding keywords and identifier

--- token/token.go
+++ token/token.go
@@ -31,5 +31,16 @@ const(
 	// Keywords
 	FUNCTION = "FUNCTION"
 	LET = "LET"
-
 )
+
+var keywords = map[string]TokenType{
+	"fn": FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string)TokenType{
+	if tok, ok := keywords[ident];ok{
+		return tok
+	}
+	return IDENT
+}
