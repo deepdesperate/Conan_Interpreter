ecb963a902d9408dd3ee7d3bd0e6d6dac58ad6d5
Naman Deep <namandeep444@gmail.com>
2024-10-02 12:39:31 +0530

    FunctionLiterals Parsing

--- ast/ast.go
+++ ast/ast.go
@@ -2,7 +2,7 @@ package ast
 
 import (
 	"bytes"
-
+	"strings"
 	"github.com/deepdesperate/Conan_Interpreter/token"
 )
 
@@ -236,5 +236,29 @@ func (bs*BlockStatement) String() string {
 		out.WriteString(s.String())
 	}
 
+	return out.String()
+}
+
+type FunctionLiteral struct {
+	Token      token.Token
+	Parameters []*Identifier
+	Body       *BlockStatement 
+}
+
+func(fl *FunctionLiteral) expressionNode() {}
+func(fl *FunctionLiteral) TokenLiteral()  string { return fl.Token.Literal}
+func(fl *FunctionLiteral) String() string {
+	var out bytes.Buffer
+
+	params:= []string{}
+	for _,p:=range fl.Parameters{
+		params = append(params, p.String())
+	}
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ","))
+	out.WriteString(")")
+	out.WriteString(fl.Body.String())
+	
 	return out.String()
 }
