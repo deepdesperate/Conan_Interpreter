f69d822ddfc31db52bf9394b226c47658cdcf11a
Naman Deep <namandeep444@gmail.com>
2024-10-14 11:15:32 +0530

    Parsing ArrayIndex Expression

--- parser/parser.go
+++ parser/parser.go
@@ -40,6 +40,7 @@ const(
 	PRODUCT // *
 	PREFIX // -X or !X
 	CALL // myFunction(X)
+	INDEX // array[index]
 
 )
 
@@ -54,6 +55,7 @@ var precedences = map[token.TokenType]int {
 	token.SLASH: PRODUCT,
 	token.ASTERISK: PRODUCT,
 	token.LPAREN: CALL,
+	token.LBRACKET: INDEX,
 
 }
 
@@ -98,6 +100,7 @@ func New(l*lexer.Lexer)*Parser{
 	p.registerInfix(token.LT, p.parseInfixExpression)
 	p.registerInfix(token.GT, p.parseInfixExpression)
 	p.registerInfix(token.LPAREN, p.parseCallExpression)
+	p.registerInfix(token.LBRACKET, p.parseIndexExpression)
 
 
 	return p
@@ -505,4 +508,17 @@ func (p*Parser) parseExpressionList(end token.TokenType) []ast.Expression {
 		return nil
 	}
 	return list
+}
+
+func(p*Parser) parseIndexExpression(left ast.Expression) ast.Expression{
+	exp := &ast.IndexExpression{Token: p.curToken, Left: left}
+
+	p.nextToken()
+	exp.Index = p.parseExpression(LOWEST)
+
+	if !p.expectPeek(token.RBRACKET){
+		return nil
+	}
+
+	return exp
 }
