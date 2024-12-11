c11cc75a7d19e0ea7b2b86c070d86f12eb4b59f7
Naman Deep <namandeep444@gmail.com>
2024-12-11 23:28:22 +0530

    benchmarking tests

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -85,7 +85,7 @@ func runVmTests(t *testing.T, tests []vmTestCase) {
 			}
 
 			fmt.Printf("\n")
-			
+
 		}
 
 		vm := New(comp.Bytecode())
@@ -777,4 +777,27 @@ func TestRecursiveFunctions(t *testing.T){
 
 	runVmTests(t, tests)
 
+}
+
+func TestRecursiveFibonacci(t *testing.T){
+	tests := []vmTestCase{
+		{
+			input: `
+			let fibonacci = fn(x) {
+				if(x == 0){
+					return 0;
+				} else {
+					if( x == 1) {
+						return 1;
+					} else {
+						fibonacci(x-1) + fibonacci(x-2); 
+					}
+				}
+			};
+			fibonacci(15);
+			`,
+			expected: 610,
+		},
+	}
+	runVmTests(t, tests)
 }
